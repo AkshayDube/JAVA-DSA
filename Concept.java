f269421941d1ef271866e5ec613b25564c27694d
AkshayDube <dubeyakshay09@gmail.com>
2022-05-25 23:19:42 +0530

    Add Concept regarding How to Convert an array to List and Vive Versa

---
+++ Concept.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Concept{
+    public static void main(String[] args){
+        //Convert Array to List
+        Integer[] array={10,20,30,40,50,60,70,80,90,100};
+        List<Integer> list=new ArrayList<>(Arrays.asList(array));
+        list.forEach(e->{System.out.print(e+" ");});//for printing purpose only
+
+        //Convert List to Array
+        List<Float> lfloat=new ArrayList<>(Arrays.asList(1.1f,1.2f,1.3f));
+        Float[] farray=new Float[lfloat.size()];
+        farray=lfloat.toArray(farray);
+        System.out.println("\nArray is : "+Arrays.toString(farray));//for printing purpose only
+    }
+}
