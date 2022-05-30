f9cfb56d6f72a3e8cc55c768641920b627e2ebe2
AkshayDube <dubeyakshay09@gmail.com>
2022-05-30 23:00:09 +0530

    Add easy way to create and Initilaze list in Java

--- Concept.java
+++ Concept.java
@@ -1,6 +1,10 @@
 import java.util.*;
 public class Concept{
     public static void main(String[] args){
+    	//Easiest way to create List
+    	List<String> newlist=List.of("Akshay","Aman","Ankur","Anzar","Nityanand");
+    	newlist.forEach(element->System.out.print(element + " "));
+    	
         //Convert Array to List
         Integer[] array={10,20,30,40,50,60,70,80,90,100};
         List<Integer> list=new ArrayList<>(Arrays.asList(array));
