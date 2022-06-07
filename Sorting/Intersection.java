051f83e66764901e31b71c28b86f982b7354cfad
AkshayDube <dubeyakshay09@gmail.com>
2022-06-07 23:40:29 +0530

    Intersection of two Sorted arrays

--- Sorting/Intersection.java
+++ Sorting/Intersection.java
@@ -1,6 +1,8 @@
 //Program to find the intersection of two sorted arrays
 package Sorting;
 
+import java.util.Arrays;
+
 public class Intersection {
 
 	public static void main(String[] args) {
@@ -8,13 +10,32 @@ public class Intersection {
 		int[] arr1= {1,2,3,4,5,6,7,8};
 		int[] arr2= {1,3,6,7,8,9,10};
 		
+		if(arr1.length>=arr2.length)
 		intersect(arr1,arr2);
+		else
+		intersect(arr2,arr1);
 
 	}
 
 	public static void intersect(int[] arr1, int[] arr2) {
-		int k,i,j;
-	
+		int i=0,j=0;
+		for(int k=0;k<arr1.length;k++) {
+			if(arr1[i]<arr2[j]) {
+				i++;
+			}
+			else if(arr1[i]>arr2[j]) {
+				j++;
+			}
+			else {//arr1[i]==arr2[j]
+				if(i==0 || arr1[i]!=arr1[i-1]) {
+					System.out.print(arr1[i] + " ");
+					i++;j++;
+				}
+				else {
+					i++;j++;
+				}
+			}
+		}
 	}
 
 }
